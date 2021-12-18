4175250839a9420eed50933c8fa93bb793f17055
Pieter DG <pieter.de.geyndt@gmail.com>
2021-12-18 20:54:26 +0100

    v0.4 - Created abouts

--- about/models.py
+++ about/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Abouts(models.Model):
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=200)
-    body = models.TextField
-    quotes= models.TextField
+    body = models.TextField(default="Dit is fake bodytext, vervang mij in de admin")
+    quotes= models.TextField(default="Dit is fake quotetext, vervang mij in de admin")
 
     def __str__(self):
         return self.title
